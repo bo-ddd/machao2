16cfc245a8ba194c160751ed5972195194dffc00
bigC <814151789@qq.com>
2022-09-26 18:39:42 +0800

    配置服务端 和 调试  axios

---
+++ pages/user/UserInfo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>userinfo</h1>
+    <div>{{ username }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    return await $axios.post('http://localhost:3000/api/user/info')
+  },
+}
+</script>
