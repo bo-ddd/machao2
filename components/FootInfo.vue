5055d228f391354984897aacdac27b73721a2c85
yang <1145938287@qq.com>
2022-09-27 17:21:50 +0800

    9.27.1721

---
+++ components/FootInfo.vue
@@ -0,0 +1,62 @@
+<template>
+     <div class="foot foot-flex tip-text">
+            <div class="logo plr-8"><img class="logo-img img-s-21"
+                    src="https://assets.codemart.com/scripts/97b8f071e76549f45dfbe527295955ba.png" alt=""></div>
+            <div class="foot-text plr-8">
+                Copyright © 2014-2022 深圳市英选码市科技有限公司 版权所有 备案号：<a class="link-style"
+                    href="http://www.miitbeian.gov.cn/publish/query/indexFirst.action">粤ICP备18032316号-1</a>
+            </div>
+        </div>
+</template>
+<style scoped>
+
+.foot {
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    padding: 20px 0;
+}
+
+.foot-flex {
+    display: flex;
+    justify-content: center;
+}
+
+.foot-text {
+    text-align: center;
+}
+.tip-text {
+    color: #727F8F;
+    font-size: 14px;
+}
+
+.img-s-21 {
+    height: 21px;
+}
+
+.img-s-28 {
+    height: 28px;
+}
+
+.link-style {
+    color: #4289dc;
+}
+
+@media screen and (max-width: 500px) {
+
+.foot-text {
+    text-align: center;
+    font-size: 12px;
+}
+}
+
+@media screen and (max-width: 680px) {
+.foot {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+}
+
+</style>
--- pages/login/LoginPage.vue
+++ pages/login/LoginPage.vue
@@ -1,21 +1,18 @@
 <template>
     <div class="login-page">
-        <div class="head-nav just-between fs-14">
-            <div class="nav-left align-center">
-                <img class="logo-img mr-30 img-s-28"
-                    src="https://assets.codemart.com/scripts/97b8f071e76549f45dfbe527295955ba.png" alt="">
-                <span class="nav-left-text">
-                    互联网软件外包服务平台
-                </span>
-            </div>
-            <div class="nav-right align-center">
-                <span class="nav-right-text">
-                    还没注册账号？
-                </span>
-                <SmallButton>注册</SmallButton>
-            </div>
-        </div>
-
+        <NavCom logoUrl="https://assets.codemart.com/scripts/97b8f071e76549f45dfbe527295955ba.png">
+            <template #left-text>
+                互联网软件外包服务平台
+            </template>
+            <template #right-text>
+                还没有注册账号？
+            </template>
+            <template #button>
+                <nuxt-link :to="{name:'register-RegisterPage'}">
+                    <SmallButton>注册</SmallButton>
+                </nuxt-link>
+            </template>
+        </NavCom>
         <div class="container just-center">
             <div v-show="flag" class="con-tab1">
                 <div class="con-title mt-40 mb-14">微信扫码登录</div>
@@ -34,7 +31,7 @@
                 <div class="just-between fs-14">
                     <div class="cur-point align-center">
                         <input type="checkbox">
-                        <span class="pa-lr-8">
+                        <span class="plr-8">
                             记住我
                         </span>
                     </div>
@@ -49,21 +46,16 @@
                 </div>
             </div>
         </div>
-
-        <div class="foot foot-flex tip-text">
-            <div class="logo plr-8"><img class="logo-img img-s-21"
-                    src="https://assets.codemart.com/scripts/97b8f071e76549f45dfbe527295955ba.png" alt=""></div>
-            <div class="foot-text plr-8">
-                Copyright © 2014-2022 深圳市英选码市科技有限公司 版权所有 备案号：<a class="link-style"
-                    href="http://www.miitbeian.gov.cn/publish/query/indexFirst.action">粤ICP备18032316号-1</a>
-            </div>
-        </div>
+        <FootInfo></FootInfo>
+       
     </div>
 </template>
 <script>
 import TipInput from '~/components/TipInput.vue';
 import SubmitButton from '../../components/SubmitButton.vue';
 import SmallButton from '~/components/SmallButton.vue';
+import NavCom from '../../components/NavCom.vue';
+import FootInfo from '../../components/FootInfo.vue';
 export default {
     data() {
         return {
@@ -89,7 +81,7 @@ export default {
             }
         }
     },
-    components: { TipInput, SubmitButton, SmallButton }
+    components: { TipInput, SubmitButton, SmallButton, NavCom, FootInfo }
 }
 </script>
 <style scoped>
@@ -97,63 +89,6 @@ export default {
     position: relative;
     min-width: 320px;
     height: 100vh;
-}
-
-.logo-img {
-    display: block;
-}
-
-.head-nav {
-    height: 70px;
-    padding: 0 28px;
-    border-bottom: 2px solid #EFEFEF;
-    box-sizing: border-box;
-    box-shadow: 0px 0.5px 5px #E9E9E9;
-}
-
-.nav-left {
-    height: 100%;
-}
-
-
-.nav-right {
-    height: 100%;
-    color: #727f8f;
-}
-
-
-.foot {
-    width: 100%;
-    position: absolute;
-    bottom: 0;
-    padding: 20px 0;
-}
-
-.foot-flex {
-    display: flex;
-    justify-content: center;
-}
-
-.foot-text {
-    text-align: center;
-}
-
-.img-s-21 {
-    height: 21px;
-}
-
-.img-s-28 {
-    height: 28px;
-}
-
-.tip-text {
-    color: #727F8F;
-    font-size: 14px;
-}
-
-
-.link-style {
-    color: #4289dc;
 }
 
 
@@ -173,7 +108,6 @@ export default {
     text-decoration: underline;
     cursor: pointer;
 }
-
 
 
 .con-title {
@@ -214,36 +148,10 @@ export default {
     color: #727F8F;
 }
 
-@media screen and (max-width: 770px) {
-    .nav-left-text {
-        display: none;
-    }
-}
-
 @media screen and (max-width: 480px) {
-    .nav-right-text {
-        display: none;
-    }
-
     .tab-wrap {
         width: 100%;
     }
 }
 
-@media screen and (max-width: 500px) {
-
-    .foot-text {
-        text-align: center;
-        font-size: 12px;
-    }
-}
-
-@media screen and (max-width: 680px) {
-    .foot {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-
-}
-</style>
+</style> 
