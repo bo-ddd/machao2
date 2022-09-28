08b7782ba790e010d62bd1ed9db075946111f658
bigC <814151789@qq.com>
2022-09-28 12:29:35 +0800

    update code

--- server/db.js
+++ server/db.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql')
 
-class Database{
-    constructor(){
+class Database {
+    constructor() {
         this.connection = mysql.createConnection({
             host: 'bj-cynosdbmysql-grp-790w57fi.sql.tencentcdb.com',
             port: '26451',
             user: 'root',
             password: 'genglei-1',
             database: 'machao_official'
-        })
+        });
         this.connect();
     }
 
-    connect(){
-        this.connection.connect( (err)=> {
+    connect() {
+        this.connection.connect((err) => {
             if (err) {
                 console.error('error connecting: ' + err.stack);
                 return;
