85c4a737395e8b46f45d98cd31348b4330ee24d7
yang <1145938287@qq.com>
2022-10-21 09:00:45 +0800

    api

--- api/api.js
+++ api/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-let baseUrl = '/api';
-// let token = localStorage.getItem('token');
+const baseUrl = '/api';
 const instance = axios.create({
     baseURL: baseUrl,
     timeout: 1000, //如果接口一秒都没有返回结果，则axios会自动帮我们做一个失败(reject)的处理
@@ -21,7 +20,7 @@ export const registerApi = function (payload = {}) {
     return instance.post('/user/register', payload)
 }
 /**
- * @description 注册接口
+ * @description 登录接口
  * @param payload object
  * @param payload.username String 用户名，6-20位之间
  * @param payload.password String 密码，6-20位之间
